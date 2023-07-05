8f37ca56917c884a9d6acd78a2411cf5e7903892
caioramirodev <caioramirodev@gmail.com>
2023-07-05 19:06:06 -0300

    atualizacao do projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import People from './assets/people.svg'
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button, User } from './styles'
+import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
+
 
 const App = () => {
-  return <div>Helo World!</div>
+
+  const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
+
+
+  function addNewUser() {
+    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }])
+  }
+
+  function deleteUser (userId) {
+    const newUsers = users.filter(user => user.id !== userId);
+    
+    setUsers(newUsers);
+  }
+
+  return (
+    <Container>
+      <Image alt="logo-image" src={People} />
+      <ContainerItens>
+        <H1>Olá!</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder='Nome' />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder='Idade' />
+
+        <Button onClick={addNewUser}>Cadastrar <img alt="arrow" src={Arrow} /> </Button>
+
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser (user.id)}><img src={Trash} alt='lata-de-lixo' /></button>
+            </User>
+          ))}
+        </ul>
+
+      </ContainerItens>
+    </Container>
+  );
 }
 
 export default App
